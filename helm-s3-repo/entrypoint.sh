46a1fafc341907a74360df7685e5871a2967c62e
Kersten Burkhardt <kersten.burkhardt@collaboration-factory.de>
2020-01-09 10:17:09 +0100

    Added s3 repo installer

---
+++ helm-s3-repo/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -l
+
+echo ">>> Executing command <<<"
+echo ""
+echo ""
+
+S3_BUCKET=$INPUT_S3_BUCKET
+REPO=$INPUT_REPO
+AWS_ACCESS_KEY_ID=$INPUT_AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_ACCESS_KEY
+AWS_DEFAULT_REGION=$INPUT_AWS_DEFAULT_REGION
+
+helm plugin install https://github.com/hypnoglow/helm-s3.git
+helm repo add $REPO $S3_BUCKET
