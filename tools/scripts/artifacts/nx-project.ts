8ca3690aaee550386b1dac9b7c5b067d544b7a69
Slaven Kopic <slaven.kopic@collaboration-factory.de>
2023-08-03 13:47:40 +0200

    logs removed

--- tools/scripts/artifacts/nx-project.ts
+++ tools/scripts/artifacts/nx-project.ts
@@ -272,22 +272,12 @@ export class NxProject {
 
   public getPathToProjectInDist(): string {
     const nestedPath = this.pathToProject;
-    console.log('getPathToProjectInDist nestedPath', nestedPath);
-    const projectType = this.nxProjectKind === NxProjectKind.Application ? 'apps' : 'libs';
-    const subPath = nestedPath
-      ? nestedPath
-      : path.join(
-            projectType,
-          this.name
-        );
-    console.log('getPathToProjectInDist subPath', subPath);
+    const projectType =
+      this.nxProjectKind === NxProjectKind.Application ? 'apps' : 'libs';
+    const subPath = nestedPath ? nestedPath : path.join(projectType, this.name);
     const base = subPath.split(projectType)[0];
     const relativePath = subPath.split(projectType)[1];
-    console.log('getPathToProjectInDist base', base);
-    console.log('getPathToProjectInDist base', relativePath);
-    const distPath = path.join(base, 'dist', projectType, relativePath);
-    console.log('getPathToProjectInDist distPath', distPath);
-    return distPath;
+    return path.join(base, 'dist', projectType, relativePath);
   }
 
   public getNpmrcPathInDist() {
