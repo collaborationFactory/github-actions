20f8db55596d682b93cb86e8d99ad4e93344b9f5
undefined <undefined>
2024-01-18 16:35:13 +0100

    revert

--- tools/scripts/upmerge/upmerge.ts
+++ tools/scripts/upmerge/upmerge.ts
@@ -24,15 +24,9 @@ export class UpmergeHandler {
       console.log('cliResult: ', cliResult);
     } catch (error) {
       const linkToAction = `https://github.com/collaborationFactory/cplace-fe/actions/runs/${process.env.GITHUB_RUN_ID}`;
-      let threadMessage = '';
-      if (typeof error === "string") {
-        threadMessage = error.toUpperCase()
-      } else if (error instanceof Error) {
-        threadMessage = error.message
-      }
       return {
         message: `There was an error running cplace-cli in repo ${repo}:\n\n${linkToAction}`,
-        threadMessage: threadMessage
+        threadMessage: error.message
       };
     }
     const index = cliResult.findIndex(v => v.includes("have been merged"));
