dccb06bdc98fb520f69f992185632ab6552ba556
senthanal <senthanal@gmail.com>
2024-11-21 16:11:38 +0100

    PFM-ISSUE-28366 fix npm registry

--- tools/scripts/artifacts/configuration.ts
+++ tools/scripts/artifacts/configuration.ts
@@ -1,3 +1,3 @@
 export function getNpmRegistryRepo(): string {
-  return process.env.NPM_REGISTRY || 'cplace-npm-local';
+  return process.env.NPM_REGISTRY !== undefined ? process.env.NPM_REGISTRY : 'cplace-npm-local';
 }
