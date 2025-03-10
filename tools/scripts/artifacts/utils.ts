bc866c0505006775fca06886ab3b27a484ef3ccc
alekhya-cplace <alekhya.jammalamadaka@cplace.com>
2025-03-10 19:53:22 +0100

    modified timestamp

--- tools/scripts/artifacts/utils.ts
+++ tools/scripts/artifacts/utils.ts
@@ -298,8 +298,8 @@ export class Utils {
       ) {
         fs.writeFileSync(
           gitHubCommentsFile,
-          `:tada: Snapshots of the following projects have been published: \n
-                 Last updated: ${Date} \n`
+          `:tada: Snapshots of the following projects have been published:
+                 Last updated: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} \n`
         );
       }
       fs.appendFileSync(gitHubCommentsFile, `${message}\n`);
