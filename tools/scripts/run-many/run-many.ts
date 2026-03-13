9b1d2dbc9895593e94088a79a0ae683fd1f9ef3a
Vladimir Arsov <vladimir.arsov@collaboration-factory.de>
2026-03-13 10:26:55 +0100

    Log the determined default branch

--- tools/scripts/run-many/run-many.ts
+++ tools/scripts/run-many/run-many.ts
@@ -57,6 +57,7 @@ function main() {
   if (!/\b[0-9a-f]{5,40}\b/.test(base)) base = 'origin/' + base;
   if (base.includes('0000000000000000')) {
     const defaultBranch = process.env.DEFAULT_BRANCH || 'master';
+    core.info(`Using default branch ${defaultBranch} as base`);
     base = `origin/${defaultBranch}`;
   }
   const ref = process.argv[6];
